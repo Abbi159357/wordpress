56490bf433cd0f3af0aff098cc67f642e29d5bf8
Abbiraam Sinnathamby <169357@student.horizoncollege.nl>
2023-06-20 09:23:33 +0200

    standaard theme

--- wp-content/themes/my_theme/index.php
+++ wp-content/themes/my_theme/index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 
     <meta charset="UTF-8">
@@ -11,7 +10,7 @@
 
     <!-- De onderstaande regel code gebruikt de WordPress-functie get_stylesheet_uri() om de URL van het huidige thema's style.css-bestand op te halen en dit te koppelen aan het stijlblad van je website. -->
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-
+    
 </head>
 
 <body>
@@ -20,53 +19,28 @@
         <!-- De onderstaande regel code gebruikt de WordPress-functie  -->
         <h1><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
         <nav>
-            <div id="navbar">
-                <a href=" <?php
-                            wp_nav_menu(array('theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'site-navigation'));
-                            ?>" id="logo">CompanyLogo</a>
-                <div id="navbar-right">
-                <?php
-             wp_nav_menu(array('theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'site-navigation'));
-            ?>
-                </div>
-            </div>
-            <script>
-                // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-                window.onscroll = function() {
-                    scrollFunction()
-                };
 
-                function scrollFunction() {
-                    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-                        document.getElementById("navbar").style.padding = "30px 10px";
-                        document.getElementById("logo").style.fontSize = "25px";
-                    } else {
-                        document.getElementById("navbar").style.padding = "80px 10px";
-                        document.getElementById("logo").style.fontSize = "35px";
-                    }
-                }
-            </script>
             <!-- De onderstaande regel code gebruikt de WordPress-functie  -->
             <?php
-            // wp_nav_menu(array('theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'site-navigation'));
+                 wp_nav_menu(array('theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'site-navigation'));
             ?>
         </nav>
     </header>
 
     <main>
-        <!-- De onderstaande regels code kijkt of en hoeveel posts er zijn en plaatst deze op de pagina.  -->
-        <?php
+    <!-- De onderstaande regels code kijkt of en hoeveel posts er zijn en plaatst deze op de pagina.  -->
+    <?php
         if (have_posts()) :
             while (have_posts()) :
                 the_post();
-        ?>
+                ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                     <div class="entry-content">
                         <?php the_content(); ?>
                     </div>
                 </article>
-            <?php
+                <?php
             endwhile;
         else :
             ?>
@@ -75,8 +49,7 @@
     </main>
 
     <footer>
-        <!-- Voeg hier je footerinhoud toe -->
+            <!-- Voeg hier je footerinhoud toe -->
     </footer>
 </body>
-
 </html>
