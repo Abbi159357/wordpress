15d83ef737e47560224f2742b9e1fee4857f75fa
Abbiraam Sinnathamby <169357@student.horizoncollege.nl>
2023-06-21 11:24:53 +0200

    basis layout

--- wp-content/themes/my_theme/index.php
+++ wp-content/themes/my_theme/index.php
@@ -21,9 +21,9 @@
         <nav>
 
             <!-- De onderstaande regel code gebruikt de WordPress-functie  -->
-            <?php
+            <ul><li><?php
                  wp_nav_menu(array('theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'site-navigation'));
-            ?>
+            ?></li></ul>
         </nav>
     </header>
 
@@ -35,11 +35,15 @@
                 the_post();
                 ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <h2 class="entry-title"><a class="posts-title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                     <div class="entry-content">
                         <?php the_content(); ?>
+                        <?php if (has_post_thumbnail()) : ?>
+                            <img src="<?php the_post_thumbnail_url(); ?>" alt="Post Image" width="200">
+                        <?php endif; ?>
                     </div>
                 </article>
+
                 <?php
             endwhile;
         else :
@@ -48,8 +52,39 @@
         <?php endif; ?>
     </main>
 
-    <footer>
-            <!-- Voeg hier je footerinhoud toe -->
-    </footer>
+    <!-- <footer> -->
+    <footer class="footer">
+    <div class="waves">
+      <div class="wave" id="wave1"></div>
+      <div class="wave" id="wave2"></div>
+      <div class="wave" id="wave3"></div>
+      <div class="wave" id="wave4"></div>
+    </div>
+    <ul class="social-icon">
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-facebook"></ion-icon>
+        </a></li>
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-twitter"></ion-icon>
+        </a></li>
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-linkedin"></ion-icon>
+        </a></li>
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-instagram"></ion-icon>
+        </a></li>
+    </ul>
+    <ul class="menu">
+     <!-- De onderstaande regel code gebruikt de WordPress-functie  -->
+     <li><?php
+                 wp_nav_menu(array('theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'site-navigation'));
+            ?></li>
+
+    </ul>
+    <p>&copy;2023 Abbi Sinnathamby | All Rights Reserved</p>
+  </footer>
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <!-- </footer> -->
 </body>
 </html>
